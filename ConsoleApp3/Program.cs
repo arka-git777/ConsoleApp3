fd6fee57e08f821ecdf031aa53aa817df3ad9de0
arka-git777 <volynetsarkadii@gmail.com>
2025-03-22 11:57:02 +0100

    delete student

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -16,7 +16,7 @@
             //var students = context.Students.ToList();
             while (true)
             {
-                Console.WriteLine("1 - Enter data\n2 - See data ");
+                Console.WriteLine("1 - Enter data\n2 - See data\n3 - If you want to delete student\n4 - If you want to change information about student");
                 int a = Convert.ToInt32(Console.ReadLine());
                 if (a == 1)
                 {
@@ -42,13 +42,46 @@
                 }
                 else if (a == 2)
                 {
-                    var students = context.Students.ToList();
-                    foreach (var s in students)
+                    foreach (var s in context.Students.ToList())
                     {
 
                         Console.WriteLine($"{s.Id}: {s.Name} {s.age} {s.Email}");
                     }
                     Console.ReadLine();
+                }
+                else if(a == 3)
+                {
+                    /*Console.Write("Enter name or age or email of the student which you want to delete - ");
+                    string ch = Console.ReadLine();
+                    var studentsDb = context.Students.ToList();
+                    var remove = studentsDb
+                        .Where(s => s.Name == ch || s.age.ToString() == ch || s.Email == ch)
+                        .ToList();
+                    context.RemoveRange(remove);
+                    context.SaveChanges();*/
+                    Console.Write("Enter name or age or email of the student which you want to delete - ");
+                    string ch = Console.ReadLine();
+                    foreach (var s in context.Students.ToList())
+                    {
+                        if (s.Name == ch || s.age.ToString() == ch || s.Email == ch)
+                        {
+                            context.Remove(s);
+                            context.SaveChanges();
+                            foreach (var j in context.Students)
+                            {
+                                if (j.Id > s.Id) 
+                                {
+                                    j.Id--;
+                                }
+                            }
+                            context.SaveChanges();
+                            break;
+                        }
+                        else if (s.Id == context.Students.Max(s => s.Id))
+                        {
+                            Console.WriteLine("Student wasn't found");
+                        }
+                    }
                 }
                 else
                     break;
